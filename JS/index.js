01e04954c780763cf49241fbfb80bf613d870a92
andres <afmartinez099@misena.edu.co>
2020-12-05 00:13:15 -0500

    Ejercicio 2

--- JS/index.js
+++ JS/index.js
@@ -4,4 +4,16 @@ var motor;
     switch (motor) {default: alert ("no existe un valor valido para tipo de bomba"); break;
     case 0: alert ("no hay establecido un valor definido para el tipo de bomba"); break;
     case 1: alert ("la bomba es una bomba de agua"); break; case 2: alert ("la bomba es una bomba de gasolina"); break;
-    case 3: alert ("la bomba es una bomba de hormigon"); break;case 4: alert ("la bomba es una bomba de pasta alimenticia"); break;}
+    case 3: alert ("la bomba es una bomba de hormigon"); break;case 4: alert ("la bomba es una bomba de pasta alimenticia"); break;};
+
+    
+{let = producto = prompt ("tipo producto"); let preciosin = prompt ("Ingrese el valor sin IVA");
+
+producto = parseInt (producto) 
+preciosin = parseInt (preciosin)
+
+if (producto == 1) {console.log ("valor con iva" + ((preciosin * 0.21) + preciosin) );}
+
+else if (producto == 2) {console.log ("valor con iva" + (preciosin * 0.10) + preciosin);}
+
+else if (producto == 3)  console.log ("valor con iva" + (preciosin * 0.05) + preciosin);}
